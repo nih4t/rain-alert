f69e033af4df6ea8e4f548720b04a9b668c74333
nih4t <nihatkky@gmail.com>
2024-09-17 23:56:57 +0200

    for alerter telegram bot added

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.getenv("API_KEY")
+LAT = 6.268720
+LON = 100.439522
+CNT = 4
+
+def bring_umbrella():
+
+    bot_token = os.getenv("bot_token")
+    bot_chatID = os.getenv("bot_chatID")
+    bot_message = "Bring umbrella, It's raining today ☂️⚡"
+    send_text = "https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=" + bot_chatID +'&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+parameters = {
+    "lat": LAT,
+    "lon": LON,
+    "appid": API_KEY,
+    "cnt": CNT
+}
+
+respond = requests.get(OWM_ENDPOINT, params=parameters)
+respond.raise_for_status()
+weather_data = respond.json()
+
+for i in range(CNT):
+    if weather_data["list"][i]["weather"][0]["id"] < 700:
+        bring_umbrella()
+        break
+
